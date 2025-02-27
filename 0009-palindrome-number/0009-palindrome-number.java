3ce810920b74be4070223b4631e1113947bef70c
Samson Mathews <samsonmmathews@gmail.com>
2025-02-27 10:18:14 -0500

    Time: 4 ms (100%), Space: 44.5 MB (18.01%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+        {
+            return false;
+        }
+        int or = x, re = 0;
+        while(x!=0)
+        {
+            re = re*10+x%10;
+            x = x/10;
+        }
+        return or == re;
+    }
+}
