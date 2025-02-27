4a3f4bdb9d5340d23f2ccb0b5c5a24fa8f3dfea7
Samson Mathews <samsonmmathews@gmail.com>
2025-02-27 17:52:31 -0500

    Time: 104 ms (42.42%), Space: 49.5 MB (35.84%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes-intArrayToBoolean.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        boolean prime[] = new boolean[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            prime[i]=true;
+        }
+        for(int i=2;i*i<=n;i++)
+        {
+            if(prime[i]==true)
+            {
+                for(int j=i*i; j<=n; j+=i)
+                {
+                    prime[j]=false;
+                }
+            }
+        }
+        for(int i=2; i<n; i++)
+        {
+            if(prime[i] == true)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
