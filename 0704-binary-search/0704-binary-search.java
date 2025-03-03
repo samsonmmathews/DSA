a501cf31e4549d5a98378ecb237ef402a665206d
Samson Mathews <samsonmmathews@gmail.com>
2025-03-03 10:38:57 -0500

    Time: 0 ms (100%), Space: 46 MB (22.76%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int size = nums.length;
+        int low = 0, high = size-1;
+        while(low <= high)
+        {
+            int mid = low+(high-low)/2;
+            if(target == nums[mid])
+            {
+                return mid;
+            }
+            else if(target < nums[mid])
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
